ae3b394c2103163be7f9a4b929d43b9d1e8652c4
mii288 <mii288@users.noreply.github.com>
2021-07-23 03:47:44 +0900

    feat: base64のコンテンツをダウンロードするための関数を追加

---
+++ sites/src/utils/download.ts
@@ -0,0 +1,15 @@
+export const download = ({
+  url,
+  filename,
+}: {
+  url: string;
+  filename: string;
+}) => {
+  const anchorEl = document.createElement('a');
+
+  anchorEl.download = filename;
+  anchorEl.href = url;
+  anchorEl.target = '_blank';
+
+  anchorEl.click();
+};

8fdb9dfbebb91670f6809e66478753e75317dc38
mii288 <mii288@users.noreply.github.com>
2021-07-23 04:08:56 +0900

    feat: PNGで保存ボタンを設置

--- sites/src/app-root.ts
+++ sites/src/app-root.ts
@@ -1,11 +1,24 @@
-import { customElement, property, LitElement, html, css, svg } from 'lit-element';
+import {
+  customElement,
+  property,
+  LitElement,
+  html,
+  svg,
+  query,
+} from 'lit-element';
 import { unsafeSVG } from 'lit-html/directives/unsafe-svg';
 import OystersLogo from '~/svg/oysters-logo-space.svg';
 import OystersIcon from '~/svg/oysters-icon-space.svg';
-import { create, cssomSheet } from 'twind';
+import { apply, create, cssomSheet } from 'twind';
+import { svg2png } from './utils/svg2png';
+import { download } from './utils/download';
 
 const sheet = cssomSheet({ target: new CSSStyleSheet() });
 const { tw } = create({ sheet });
+
+const styles = {
+  button: apply`bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 rounded`,
+};
 
 @customElement('app-root')
 export class AppRoot extends LitElement {
@@ -14,8 +27,19 @@ export class AppRoot extends LitElement {
     icon: OystersIcon,
   };
 
+  @property() type = 'logo' as 'logo' | 'icon';
+  @query('#canvas svg') svgEl!: SVGSVGElement;
+
   static get styles() {
     return [sheet.target];
+  }
+
+  private async download(e: Event) {
+    e.preventDefault();
+    const url = await svg2png(this.svgEl);
+    const filename = `oysters-${this.type}-space`;
+
+    download({ url, filename });
   }
 
   render() {
@@ -23,6 +47,12 @@ export class AppRoot extends LitElement {
       <div class="${tw`flex`}" id="canvas">
         ${svg`${unsafeSVG(this.brand[this.type])}`}
       </div>
+      <button
+        class="${tw(styles.button)}"
+        @click="${(e: Event) => this.download(e, 'png')}"
+      >
+        Download PNG
+      </button>
     `;
   }
 }

c06451bf4b2f912e6ba8188be8674ec550781629
mii288 <mii288@users.noreply.github.com>
2021-07-23 02:26:06 +0900

    feat: setup tailwindcss

--- sites/src/app-root.ts
+++ sites/src/app-root.ts
@@ -1,25 +1,26 @@
 import { customElement, property, LitElement, html, css, svg } from 'lit-element';
-import { unsafeSVG } from 'lit-html/directives/unsafe-svg'
-import OystersLogo from '~/svg/oysters-logo.svg'
-import OystersIcon from '~/svg/oysters-icon-space.svg'
+import { unsafeSVG } from 'lit-html/directives/unsafe-svg';
+import OystersLogo from '~/svg/oysters-logo-space.svg';
+import OystersIcon from '~/svg/oysters-icon-space.svg';
+import { create, cssomSheet } from 'twind';
+
+const sheet = cssomSheet({ target: new CSSStyleSheet() });
+const { tw } = create({ sheet });
 
 @customElement('app-root')
 export class AppRoot extends LitElement {
   @property() brand = {
     logo: OystersLogo,
-    icon: OystersIcon
-  }
-
-  @property() type = 'logo' as 'logo' | 'icon'
+    icon: OystersIcon,
+  };
 
   static get styles() {
-    return css`
-    `;
+    return [sheet.target];
   }
 
   render() {
     return html`
-      <div class="wrapper">
+      <div class="${tw`flex`}" id="canvas">
         ${svg`${unsafeSVG(this.brand[this.type])}`}
       </div>
     `;
